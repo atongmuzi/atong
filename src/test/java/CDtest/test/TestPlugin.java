3511f49bb29c1614baf0c8b8a3652eb9fa2dfaea
atongmuzi <jazx505@163.com>
2019-08-21 09:23:05 +0800

    add test

--- src/test/java/CDtest/test/TestPlugin.java
+++ src/test/java/CDtest/test/TestPlugin.java
@@ -1,4 +1,43 @@
 package CDtest.test;
 
+import org.apache.ibatis.plugin.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class TestPlugin {
+    @Test
+    public  void  PluginInterceptGet(){
+        Map map = new HashMap();
+        map = (Map) new AlwaysMapPlugin().plugin(map);
+        Assert.assertEquals("Always",map.get("Anything"));
+    }
+    @Test
+    public void shouldNotInterceptToString() {
+        Map map = new HashMap();
+        map = (Map) new AlwaysMapPlugin().plugin(map);
+        Assert.assertFalse("Always".equals(map.toString()));
+    }
+
+    @Intercepts({@Signature(type = Map.class,method = "get",args = {Object.class})})
+    public static class  AlwaysMapPlugin implements Interceptor{
+
+        @Override
+        public Object intercept(Invocation invocation) throws Throwable {
+            return "Always";
+        }
+
+        @Override
+        public Object plugin(Object target) {
+            return Plugin.wrap(target,this);
+        }
+
+        @Override
+        public void setProperties(Properties properties) {
+
+        }
+    }
 }
