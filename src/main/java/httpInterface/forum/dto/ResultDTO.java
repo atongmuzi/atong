07d05e33b772637c5ea7e711ccfa5951efe8055c
atongmuzi <jazx505@163.com>
2020-03-07 17:27:39 +0800

    modify the code

--- src/main/java/httpInterface/forum/dto/ResultDTO.java
+++ src/main/java/httpInterface/forum/dto/ResultDTO.java
@@ -3,6 +3,11 @@ package httpInterface.forum.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * @author tongcy
+ * @date 2020-03-07 17:12
+ * @desc 论坛结果集对象
+ **/
 @Getter@Setter
 public class ResultDTO {
     public String code;
