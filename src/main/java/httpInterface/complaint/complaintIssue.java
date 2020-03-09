86637ef2f1f62994a53b8f6a0d0ce1448fbc2c4b
atongmuzi <jazx505@163.com>
2020-03-09 14:35:12 +0800

    add complaint interface

---
+++ src/main/java/httpInterface/complaint/complaintIssue.java
@@ -0,0 +1,11 @@
+package httpInterface.complaint;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class complaintIssue {
+    private String code ;
+    private String msg;
+    private Integer time;
+}
