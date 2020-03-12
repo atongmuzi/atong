342f3ce43abde88150e05b2a17ea5a56fd2e43cc
atongmuzi <jazx505@163.com>
2020-03-12 16:25:23 +0800

    abc

--- src/main/java/httpInterface/complaint/testNumber.java
+++ src/main/java/httpInterface/complaint/testNumber.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class testNumber {
     public static void main(String[] args) {
-        double[] dbe = new double[]{1,2,3,4,5,6,7,8,9,0};
-        System.out.println(Arrays.toString(fourSum(dbe, 20)));
+        double[] dbe = new double[]{90.14,83.16,89.22,92,88.46,77.1,85.44,92.88,86.26,77.86,46.04,83.7,76.68,76.28,78.76,83.94,71.94,86.16,72.38};
+        System.out.println(Arrays.toString(fourSum(dbe, 356.96)));
     }
 
     public static double[] fourSum(double[] arr, double sum)
