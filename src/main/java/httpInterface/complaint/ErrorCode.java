5e904b443e571a9ddcc4d912e2265fb414c0d5f1
atongmuzi <jazx505@163.com>
2020-03-18 16:31:08 +0800

    add enum

--- src/main/java/httpInterface/complaint/ErrorCode.java
+++ src/main/java/httpInterface/complaint/ErrorCode.java
@@ -1,4 +1,36 @@
 package httpInterface.complaint;
-
 public enum ErrorCode {
+    SUCCESS(0, "成功");
+    private  int errorNo;
+    private String errorMsg;
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+
+    public int getErrorNo() {
+        return errorNo;
+    }
+
+    public void setErrorNo(int errorNo) {
+        this.errorNo = errorNo;
+    }
+
+    ErrorCode(int errorNo, String errorMsg) {
+        this.errorNo = errorNo;
+        this.errorMsg = errorMsg;
+    }
+
+    ErrorCode() {
+        this.errorNo = 100;
+        this.errorMsg = "未知错误";
+    }
+
+
+
 }
+
